77a17564d8f9ed2cef59d8eb2e71bdf8dfe1fad5
muhamadabduh <muhamad.abduh.muh@gmail.com>
2018-10-25 14:24:13 +0700

    add seeders with faker. please do npm install after you pull

---
+++ seeders/20181025070736-post-seeder.js
@@ -0,0 +1,44 @@
+'use strict';
+const num = 10
+console.log(process.argv)
+const faker = require('faker')
+function generateFakePost(num) {
+  let posts = []
+  for (let i = 0; i < num; i++) {
+    let obj = {
+      title: faker.lorem.sentence(),
+      image: faker.image.nature(),
+      review: faker.lorem.text(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    posts.push(obj)
+  }
+  return posts
+}
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Posts', generateFakePost(num))
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Posts', null)
+  }
+};
