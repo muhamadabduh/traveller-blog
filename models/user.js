78265b0bb02593253bedf3d0a52f744c0114d370
muhamadabduh <muhamad.abduh.muh@gmail.com>
2018-10-26 00:49:56 +0700

    login and change template admin

--- models/user.js
+++ models/user.js
@@ -1,11 +1,26 @@
 'use strict';
+const bcrypt = require('bcryptjs')
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {});
+    role: DataTypes.STRING, 
+    email : {
+      type : DataTypes.STRING, 
+      validate : {
+        isEmail : true
+      }
+    }
+  }, {
+    hooks : {
+      beforeCreate(user, options){
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(user.password, salt)
+        user.password = hash
+      }
+    }
+  });
   User.associate = function (models) {
     // associations can be defined here
     User.hasMany(models.Post)
--- routers/users/index.js
+++ routers/users/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const Model = require('../../models/index')
+const UserController = require('../../controllers/UserController')
 
 
 router.get('/', (req,res) => [
     res.render('users/index')
 ])
+
+router.get('/register', UserController.register)
+router.post('/add', UserController.store)
+router.get('/login', UserController.login)
+router.post('/login', UserController.toLogin)
 
 
 
